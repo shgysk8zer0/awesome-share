038403f00852af90fef1df31c015b6c72bf81b4d
Chris Zuber <shgysk8zer0@gmail.com>
2017-09-22 23:08:57 -0700

    Add function to store options from FormData

--- js/options.js
+++ js/options.js
@@ -1,1 +1,8 @@
-import {shares} from './consts.js';
+// import {shares} from './consts.js';
+export function setOpts(form) {
+	const obj = {};
+	[...form.keys()].forEach(key => {
+		obj[key] = form.get(key);
+	});
+	browser.storage.local.set(obj);
+}
